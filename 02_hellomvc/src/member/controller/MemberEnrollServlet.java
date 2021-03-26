55e45946e2452973ee07bea1e5eb3586c817f5b8
ksh940911 <ksh940911@naver.com>
2021-03-26 21:51:46 +0900

    21.03.26 COURSE

---
+++ 02_hellomvc/src/member/controller/MemberEnrollServlet.java
@@ -0,0 +1,35 @@
+package member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberEnrollServlet
+ */
+@WebServlet("/member/memberEnroll")
+public class MemberEnrollServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 회원가입페이지
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/member/memberEnroll.jsp")
+			   .forward(request, response);
+	}
+
+	/**
+	 * 회원가입 처리 - db에 저장
+	 * 
+	 * java.sql.Date객체 생성하기 : Date.valueOf("1990-09-09")
+	 * 
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ 02_hellomvc/src/member/model/service/MemberService.java
@@ -0,0 +1,24 @@
+package member.model.service;
+
+import static common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+import member.model.dao.MemberDao;
+import member.model.vo.Member;
+
+public class MemberService {
+
+	private MemberDao memberDao = new MemberDao();
+
+	public static final String MEMBER_ROLE = "U";
+	public static final String ADMIN_ROLE = "A";
+	
+	public Member selectOne(String memberId) {
+		Connection conn = getConnection();
+		Member member = memberDao.selectOne(conn, memberId);
+		close(conn);
+		return member;
+	}
+	
+}
